34964bdd3c492afd13d8403b499882bdd09bee95
nuix09 <136358529+nuix09@users.noreply.github.com>
2024-08-11 17:19:25 +0530

    added date/day/month and an svg element for music notes

--- script.js
+++ script.js
@@ -15,3 +15,55 @@ menuBoxes.forEach((menuBox, index) => {
     navMenuLi[index].style.color = "";
   });
 });
+
+// function for time
+time = () => {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hour + ":" + min + ":" + sec;
+
+  document.getElementById("time").innerHTML = currentTime;
+};
+time(); //call fuction time
+setInterval(time, 1000); //refreshes the time function every 1000ms
+
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const currentDateDayMonth = new Date();
+let day = weekday[currentDateDayMonth.getDay()];
+document.getElementById("day").innerHTML = day;
+
+document.getElementById("date").innerHTML = currentDateDayMonth.getDate();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[currentDateDayMonth.getMonth()];
+document.getElementById("month").innerHTML = month;
